f266d919f16f2aa09a2ce4902c26bb82c9320a5a
Raúl <raulacostadeveloper@outlook.com>
2022-05-23 16:09:49 -0500

    manejo de bd

--- api/index.js
+++ api/index.js
@@ -1,17 +1,14 @@
-//mongodb+srv://AngelRaulAcostaRojas:Dd.-3976880@cluster0.hysjg.mongodb.net/almuerzi-db?retryWrites=true&w=majority
-//mongodb+srv://AngelRaulAcostaRojas:<password>@cluster0.hysjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//mongodb+srv://AngelRaulAcostaRojas:Dd.-3976880@cluster0.hysjg.mongodb.net/Cluster0?retryWrites=true&w=majority
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors()); 
 
 //Conexión con la base de datos //ERROR AQUÍ
-    mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true });
 
 //---Producción ------
 //Indica las rutas
@@ -26,8 +23,8 @@ app.use(cors());
 //-----------Prueba1-------------
 app.get('*', (req,res) => {
     res.send('Respuesta de Servidor')
-})
-
+});
+module.exports = app;
 //-------------Prueba2---------------
 // module.exports=(req,res)=>{
 //     res.send('Prueba Api express, mongoose, bodyParser, cors, app, app.use(bodyParser.json()); app.use(cors()); \n try {mongoose.connect} catch (error) {    module.exports=(req,res)=>{ res.send(Error con la BD) }')
